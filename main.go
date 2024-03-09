2bb72a8254d9e84e221a8ef7a4a11ba6699b06c1
shotius <sh.archemashvili@gmail.com>
2024-03-10 02:20:58 +0400

    init and user demo route

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	ID uint `json:id`
+	Name string `json:name`
+	LastName string `json:lastName`
+	PhotoUrl string `json:photoUrl`
+}
+
+var user = User{ID: 0, Name: "Shota", LastName: "Archem", PhotoUrl: ""}
+
+func getCurrentUser(c *gin.Context) {
+c.IndentedJSON(http.StatusOK, user)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/user", getCurrentUser)
+
+	router.Run("localhost:8080")
+}
